b8bb78067ba225fe7770d5a9ddc9ddcd7698b52d
Gustavo Lopes <gustavo.lopesv3@gmail.com>
2021-06-03 00:15:34 -0500

    metodo compatilhar link

--- src/components/ModalLink/index.js
+++ src/components/ModalLink/index.js
@@ -1,9 +1,36 @@
 import React from 'react'
-import { Text, TouchableOpacity, View, TouchableWithoutFeedback } from 'react-native'
+import { TouchableOpacity, View, TouchableWithoutFeedback, Share } from 'react-native'
 import {Feather} from '@expo/vector-icons'
 import {ModalContainer, Container, Header, LinkArea, Title, LongUrl, ShortLinkArea, ShortLinkUrl} from './styles'
-
+import Clipboard from 'expo-clipboard'
 export function ModalLink({onClose}){
+
+  function copyLink(){
+    Clipboard.setString('https://lopesit.com.br')
+    alert('Link copiado')
+  }
+
+  async function handleShare(){
+    try {
+      const result = await Share.share({
+        message: `Link: seulink.com`
+      })
+      if(result.action === Share.sharedAction){
+        if(result.activityType){
+          console.log('activityType')
+        }else{
+          //compartilhou
+          console.log('compartilhado com sucesso!!!')
+        }
+      }else if(result.action === Share.dismissedAction){
+        console.log('Modal fechado')
+      }
+
+    } catch (error) {
+      console.log(error.messege)
+    }
+  }
+
   return(
     <ModalContainer>
       <TouchableWithoutFeedback onPress={onClose}>
@@ -14,16 +41,16 @@ export function ModalLink({onClose}){
         <TouchableOpacity>
           <Feather name='x' color='#212743' size={30} onPress={onClose}/>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleShare}>
           <Feather name='share' color='#212743' size={30}/>
         </TouchableOpacity>
       </Header>
       <LinkArea>
         <Title>Link encurtado</Title>
         <LongUrl numberOfLines={1}>https://www.lopesit.com.br</LongUrl>
-        <ShortLinkArea activeOpacity={1} >
+        <ShortLinkArea activeOpacity={1} onPress={copyLink}>
           <ShortLinkUrl numberOfLines={1}>https://bit.ly/kwjer</ShortLinkUrl>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={copyLink}>
             <Feather name='copy' color='#fff' size={25}/>
           </TouchableOpacity>
         </ShortLinkArea>
