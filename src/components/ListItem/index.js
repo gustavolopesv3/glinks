b9f2c2b7d7176e8dc3e3ed799ced06fcaf8fbbfd
Gustavo Lopes <gustavo.lopesv3@gmail.com>
2021-06-03 20:24:11 -0500

    abrindo modal com dado do link selecionado

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import {Text, View} from 'react-native'
 import {Feather} from '@expo/vector-icons'
 import {ContainerButton, Item} from './styles'
-export function ListItem({data}){
+export function ListItem({data, selectedItem}){
   return(
     <View>
-      <ContainerButton activeOpacity={0.8} onPress={()=>{}}>
+      <ContainerButton activeOpacity={0.8} onPress={()=>selectedItem(data)}>
         <Feather 
           name='link'
           color='#fff'
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,4 +1,6 @@
 import React,{useState, useEffect} from 'react'
+import {Modal} from 'react-native'
+import {ModalLink} from '../../components/ModalLink'
 import {StatusBarPage} from '../../components/StatusBarPage'
 import { Container, Title,ListLinks } from './styles'
 import { Menu } from '../../components/Menu'
@@ -6,13 +8,14 @@ import { ListItem } from '../../components/ListItem'
 import {useIsFocused} from '@react-navigation/native'
 import {getLinksSave} from '../../utils/storageLinks'
 
+
 export function MyLinks(){
 
   const isFocused = useIsFocused()
 
   const [links, setLinks] = useState([])
   const [dataLink, setDataLink] = useState({})
-  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [modalVisible, SetModelVisible] = useState(false)
 
   useEffect(()=>{
     async function getLinks(){
@@ -22,9 +25,13 @@ export function MyLinks(){
     getLinks()
   },[isFocused])
 
-  return(
+  function handleItem(item){
+    setDataLink(item)
+    SetModelVisible(true)
+  }
 
-      
+  return(
+  
         <Container>
           <StatusBarPage 
           barStyle='light-content'
@@ -36,10 +43,13 @@ export function MyLinks(){
           <ListLinks
           data={links}
           keyExtractor={(item)=> String(item.id)}
-          renderItem={({item})=> <ListItem data={item}/>}
+          renderItem={({item})=> <ListItem data={item} selectedItem={handleItem}/>}
           contentContainerStyle={{paddingBottom:20}}
           showsVerticalScrollIndicator={false}
           />
+          <Modal visible={modalVisible} transparent animationType='slide'>
+            <ModalLink onClose={()=>SetModelVisible(false)} linkEncurtado={dataLink}/>
+          </Modal>
         </Container>
 
 
